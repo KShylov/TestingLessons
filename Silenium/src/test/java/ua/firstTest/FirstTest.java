0791b0b0d15ea0d8b8d6dc9bcc37070551489560
KShylov <kirpichk@gmail.com>
2019-12-17 18:25:39 +0200

    refactoring

--- Silenium/src/test/java/ua/firstTest/FirstTest.java
+++ Silenium/src/test/java/ua/firstTest/FirstTest.java
@@ -1,37 +1,50 @@
 package ua.firstTest;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.assertTrue;
 
 public class FirstTest
 {
+  WebDriver driver;
+  private static final String START_URL = "https://www.google.com.ua";
+  private static final String GOOGLE_SEARCH = "q";
+  private static final String FIRST_LINK = "//*[@id=\"rso\"]/div/div/div[1]/div/div/div[1]/a";
+  private static final String WIKIPEDIA_SEARCH = "search";
+  @BeforeTest
+  public void beforeFirstTest(){
+    System.setProperty("webdriver.chrome.driver","D:\\testfolder\\chromedriver.exe");
+    driver = new ChromeDriver();
+  }
   @Test
   public void firstTest(){
-    System.setProperty("webdriver.chrome.driver","D:\\testfolder\\chromedriver.exe");
-    ChromeDriver driver = new ChromeDriver();
-    driver.get("https://www.google.com.ua");
-
+    driver.get(START_URL);
     WebElement element;
-    element = driver.findElement(By.name("q"));
+    element = driver.findElement(By.name(GOOGLE_SEARCH));
     element.sendKeys("Википедия");
     element.submit();
-    element = driver.findElement(By.xpath("//*[@id=\"rso\"]/div/div/div[1]/div/div/div[1]/a"));
+
+    element = driver.findElement(By.xpath(FIRST_LINK));
     element.click();
-    element = driver.findElement(By.name("search"));
+
+    assertTrue(driver.getCurrentUrl().contains("https://ru.wikipedia.org"));
+
+    element = driver.findElement(By.name(WIKIPEDIA_SEARCH));
     element.sendKeys("Selenium");
     element.submit();
+
     element = driver.findElement(By.linkText("Apache License 2.0"));
     String expect = element.getText();
-    System.out.println(expect);
-    Assert.assertTrue(expect.equals("Apache License 2.0"));
-
-    //driver.quit();
-
+    assertTrue(expect.equals("Apache License 2.0"));
+  }
+  @AfterTest
+  public void afterFirstTest(){
+    driver.quit();
   }
 }
